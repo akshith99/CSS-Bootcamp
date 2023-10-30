8dad5f3124025b4e5773266b96004523ade9d378
akshith99 <ak38293@gmail.com>
2023-10-30 07:25:31 +0530

    first commit

---
+++ basics-understanding-the-course-project/shared.js
@@ -0,0 +1,72 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
+
+// console.dir(backdrop);
+// console.dir(selectPlanButton);
+
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function () {
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; // This will actually overwrite the complete class list
+        modal.classList.add('open');
+        backdrop.computedStyleMap.display = 'block';
+        setTimeout(() => {
+            backdrop.classList.add('open');
+        }, 10);
+    })
+}
+
+backdrop.addEventListener('click', function () {
+    // mobileNav.style.display = 'none';
+
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+
+    setTimeout(() => {
+        backdrop.style.display = 'none';
+    }, 200);
+}
+
+toggleButton.addEventListener('click', function () {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+
+    mobileNav.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(() => {
+        backdrop.classList.add('open');
+    }, 10);
+})
+
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('Animation started', event);
+});
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('Animation ended', event);
+})
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log('Animation iteration', event);
+})
